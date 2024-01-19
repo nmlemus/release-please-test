502365e547062c2bd1b36984afd9c28f5a3d8745
Noel Moreno Lemus <nmlemus@gmail.com>
2024-01-19 16:56:44 -0500

    feat(main.py): mockup for the main methods to play with Azure Storage

--- src/code/main.py
+++ src/code/main.py
@@ -1,1 +1,20 @@
 print("Hola")
+print("Mundo")
+
+# Implement all the functionaities to download blob file from Azure Storage
+
+
+def download_blob_file():
+    pass
+
+
+def upload_blob_file():
+    pass
+
+
+def list_blobs():
+    pass
+
+
+def delete_blob():
+    pass
